5ba3041ab1090164705d67d4b95d997479b78deb
Michael Nathanson <michael.nathanson@gmail.com>
2017-03-18 14:31:58 -0400

    Removing wordhop reply

--- src/wordhop.js
+++ src/wordhop.js
@@ -66,24 +66,7 @@ class Wordhop {
     if (user) {
         message.user_profile = user
     }
-    return this.wordhop.hopIn(message, {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "button",
-          "text": "What do you want to do next?",
-          "buttons": [{
-            "type": "web_url",
-            "url": "https://petersapparel.parseapp.com",
-            "title": "Show Website"
-          }, {
-            "type": "postback",
-            "title": "Start Chatting",
-            "payload": "USER_DEFINED_PAYLOAD"
-          }]
-        }
-      }
-    })
+    return this.wordhop.hopIn(message)
   }
 
   hopOut(platform, message) {
